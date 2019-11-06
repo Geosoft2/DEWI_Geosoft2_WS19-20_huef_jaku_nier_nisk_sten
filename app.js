55b2c35efec8efe144eda8f64df626d6de2ed518
Delucse <46593742+Delucse@users.noreply.github.com>
2019-11-06 13:59:09 +0100

    initialize EJS-View

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 
 function connectMongoDB() {
@@ -54,7 +53,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -68,7 +67,6 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist')
 app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
