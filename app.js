4d329220739c69e93e5a6542f37f8e79aecce3a6
Delucse <46593742+Delucse@users.noreply.github.com>
2019-11-06 13:10:50 +0100

    initialization of the server and mongoDB

---
+++ app.js
@@ -0,0 +1,89 @@
+// jshint esversion: 8
+// jshint node: true
+"use strict";
+
+
+// server uses port 3000
+const port = 3000;
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+function connectMongoDB() {
+  (async () => {
+    // set up default ("Docker") mongoose connection
+    await mongoose.connect('mongodb://mongo/itemdb', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      autoReconnect: true
+    }).then(db => {
+      console.log('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""');
+    }).catch(async err => {
+      console.log('Connection to '+'mongodb://mongo/itemdb'+' failed, try to connect to '+'mongodb://localhost:27017/itemdb');
+      // set up "local" mongoose connection
+      await mongoose.connect('mongodb://localhost:27017/itemdb', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        autoReconnect: true
+      }).then(db => {
+        console.log('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""');
+      }).catch(err2nd => {
+        console.log('Error at MongoDB-connection with Docker: '+err);
+        console.log('Error at MongoDB-connection with Localhost: '+err2nd);
+        console.log('Retry to connect in 3 seconds');
+        setTimeout(connectMongoDB, 3000); // retry until db-server is up
+      });
+    });
+  })();
+}
+
+// connect to MongoDB
+connectMongoDB();
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// make packages available for client using statics
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
