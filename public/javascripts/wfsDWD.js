7012e2c92ac2062349e57cb7badd425399daaaed
phuef <phil.hueffer@gmail.com>
2019-11-27 16:42:53 +0100

    calculating circles

--- public/javascripts/wfsDWD.js
+++ public/javascripts/wfsDWD.js
@@ -64,42 +64,20 @@ var radarlayer;
  */
 function mapExtendChange(bounds) {
     var bbox = boundingbox(bounds);
-    var bboxesArray =bboxes(bbox.bbox);
     requestExtremeWeather(bbox);
+    //updateTwitterStream(bbox.bbox);
 }
 
-/**
- * returns an array with boundingboxes smaller than 25x25 miles that cover the whole given bounding box
- * @param bbox a bounding box with a southWest and northEast coordinate
- */
-function bboxes(bbox){
-    var westOst= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.southWest.lat, bbox.northEast.lng));
-    var nordSued= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.northEast.lat, bbox.southWest.lng));
-    var westOstDiff= Math.ceil(westOst/25);
-    var nordSuedDiff= Math.ceil(nordSued/25);
-    var boxes=[];
-    var lngMultiplicator= (bbox.northEast.lng - bbox.southWest.lng)/westOstDiff;
-    var latMultiplicator= (bbox.northEast.lat - bbox.southWest.lat)/nordSuedDiff;
-    boxes.push(westOstDiff);
-    for (var i=0; i<westOstDiff; i++){
-        for (var j=0; j<nordSuedDiff;j++){
-            boxes.push(
-                {southWest:{
-                lat:bbox.southWest.lat + j*latMultiplicator,
-                lng:bbox.southWest.lng + i*lngMultiplicator
-            },
-              northEast:{
-                  lat:bbox.southWest.lat + (j+1)*latMultiplicator,
-                  lng:bbox.southWest.lng + (i+1)*lngMultiplicator
-              }
-                }
-            );
-        }
-    }
-    console.log(boxes);
-    return boxes;
-
+function updateTwitterStream(bbox) {
+    $.ajax({
+        type: "POST",
+        url: '/api/v1/twitter/setStreamFilter',
+        // contentType: "application/json",
+        dataType: 'json',
+        data: bbox
+    })
 }
+
 /**
  * @desc creates a json within the current map-extent as coordinates
  * @param {json} bounds coordinates of current map-extent
@@ -267,65 +245,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-function meterToMile(meter){
-    return meter*0.0006213712;
-}
-function toRad(n) {
-    return n * Math.PI / 180;
-}
-
-/**
- * calculates the distance between two points in meters
- * taken from https://gist.github.com/mathiasbynens/354587
- * @param lat1
- * @param lon1
- * @param lat2
- * @param lon2
- * @returns {string|number} distance between the two points in meters
- */
-function distVincenty(lat1, lon1, lat2, lon2) {
-    var a = 6378137,
-        b = 6356752.3142,
-        f = 1 / 298.257223563, // WGS-84 ellipsoid params
-        L = toRad(lon2-lon1),
-        U1 = Math.atan((1 - f) * Math.tan(toRad(lat1))),
-        U2 = Math.atan((1 - f) * Math.tan(toRad(lat2))),
-        sinU1 = Math.sin(U1),
-        cosU1 = Math.cos(U1),
-        sinU2 = Math.sin(U2),
-        cosU2 = Math.cos(U2),
-        lambda = L,
-        lambdaP,
-        iterLimit = 100;
-    do {
-        var sinLambda = Math.sin(lambda),
-            cosLambda = Math.cos(lambda),
-            sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
-        if (0 === sinSigma) {
-            return 0; // co-incident points
-        }
-        var cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda,
-            sigma = Math.atan2(sinSigma, cosSigma),
-            sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma,
-            cosSqAlpha = 1 - sinAlpha * sinAlpha,
-            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha,
-            C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
-        if (isNaN(cos2SigmaM)) {
-            cos2SigmaM = 0; // equatorial line: cosSqAlpha = 0 (§6)
-        }
-        lambdaP = lambda;
-        lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
-    } while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
-
-    if (!iterLimit) {
-        return NaN; // formula failed to converge
-    }
-
-    var uSq = cosSqAlpha * (a * a - b * b) / (b * b),
-        A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))),
-        B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq))),
-        deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM))),
-        s = b * A * (sigma - deltaSigma);
-    return s.toFixed(3); // round to 1mm precision
 }
--- routes/api/v1/twitter/twitter.js
+++ routes/api/v1/twitter/twitter.js
@@ -68,10 +68,148 @@ const setStreamFilter = function (req, res){
 // router.post("/setStreamFilter", (req,res) => {
     var bbox = req.body.bbox;
     const rules = [];
-    if (bbox) {
-        rules.push({"value" :  " bounding_box: [" + String(bbox.southWest.lng) + " " + String(bbox.southWest.lat) + " " + String(bbox.northEast.lng) + " " + String(bbox.northEast.lat) + "]"});
+    // if (bbox) {
+    //     rules.push({"value" :  " bounding_box: [" + String(bbox.southWest.lng) + " " + String(bbox.southWest.lat) + " " + String(bbox.northEast.lng) + " " + String(bbox.northEast.lat) + "]"});
+    // }
+    var boboxes = bboxes(bbox);
+    var circles = getRadii(bbox);
+    for (var i in boboxes){
+        rules.push({"value" :  " bounding_box: ["
+                + String(boboxes[i].southWest.lng) + " "
+                + String(boboxes[i].southWest.lat)  + " "
+                + String(boboxes[i].northEast.lng) + " "
+                + String(boboxes[i].northEast.lat) + "]"});
     }
+    setRules(rules, token);4
 };
+
+/**
+ * calculates an array of circles that covers the given bounding box
+ * @param bbox
+ * @returns {[]} array with circles that have a center (lat/lng) and a radius
+ * */
+function getRadii(bbox){
+    var westOst= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.southWest.lat, bbox.northEast.lng));
+    var nordSued= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.northEast.lat, bbox.southWest.lng));
+    var westOstDiff= Math.ceil(westOst/35);
+    var nordSuedDiff= Math.ceil(nordSued/35);
+    var boxes=[];
+    var lngMultiplicator= (bbox.northEast.lng - bbox.southWest.lng)/westOstDiff;
+    var latMultiplicator= (bbox.northEast.lat - bbox.southWest.lat)/nordSuedDiff;
+    for (var i=0; i<(westOstDiff+1); i++){
+        for (var j=0; j<(nordSuedDiff+1);j++){
+            boxes.push(
+                {center:{
+                        lat:bbox.southWest.lat + j*latMultiplicator,
+                        lng:bbox.southWest.lng + i*lngMultiplicator
+                    },
+                    radius: mileToMeter(24.7485)
+
+                }
+            );
+        }
+    }
+    return boxes;
+}
+
+/**
+ * calculates an array of 25x25 boxes that cover the given bounding box
+ * @param bbox
+ * @returns {[]} array with boxes that have a southWest (lat/lng) and a northEast (lat/lng) corner
+ */
+function bboxes(bbox){
+    var westOst= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.southWest.lat, bbox.northEast.lng));
+    var nordSued= meterToMile(distVincenty(bbox.southWest.lat, bbox.southWest.lng, bbox.northEast.lat, bbox.southWest.lng));
+    var westOstDiff= Math.ceil(westOst/25);
+    var nordSuedDiff= Math.ceil(nordSued/25);
+    var boxes=[];
+    var lngMultiplicator= (bbox.northEast.lng - bbox.southWest.lng)/westOstDiff;
+    var latMultiplicator= (bbox.northEast.lat - bbox.southWest.lat)/nordSuedDiff;
+    for (var i=0; i<westOstDiff; i++){
+        for (var j=0; j<nordSuedDiff;j++){
+            boxes.push(
+                {southWest:{
+                        lat:bbox.southWest.lat + j*latMultiplicator,
+                        lng:bbox.southWest.lng + i*lngMultiplicator
+                    },
+                    northEast:{
+                        lat:bbox.southWest.lat + (j+1)*latMultiplicator,
+                        lng:bbox.southWest.lng + (i+1)*lngMultiplicator
+                    }
+                }
+            );
+        }
+    }
+    return boxes;
+
+}
+
+function meterToMile(meter){
+    return meter*0.0006213712;
+}
+
+function mileToMeter(mile) {
+    return mile/0.0006213712;
+}
+
+function toRad(n) {
+    return n * Math.PI / 180;
+}
+
+/**
+ * calculates the distance between two points in meters
+ * taken from https://gist.github.com/mathiasbynens/354587
+ * @param lat1
+ * @param lon1
+ * @param lat2
+ * @param lon2
+ * @returns {string|number} distance between the two points in meters
+ */
+function distVincenty(lat1, lon1, lat2, lon2) {
+    var a = 6378137,
+        b = 6356752.3142,
+        f = 1 / 298.257223563, // WGS-84 ellipsoid params
+        L = toRad(lon2-lon1),
+        U1 = Math.atan((1 - f) * Math.tan(toRad(lat1))),
+        U2 = Math.atan((1 - f) * Math.tan(toRad(lat2))),
+        sinU1 = Math.sin(U1),
+        cosU1 = Math.cos(U1),
+        sinU2 = Math.sin(U2),
+        cosU2 = Math.cos(U2),
+        lambda = L,
+        lambdaP,
+        iterLimit = 100;
+    do {
+        var sinLambda = Math.sin(lambda),
+            cosLambda = Math.cos(lambda),
+            sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
+        if (0 === sinSigma) {
+            return 0; // co-incident points
+        }
+        var cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda,
+            sigma = Math.atan2(sinSigma, cosSigma),
+            sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma,
+            cosSqAlpha = 1 - sinAlpha * sinAlpha,
+            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha,
+            C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
+        if (isNaN(cos2SigmaM)) {
+            cos2SigmaM = 0; // equatorial line: cosSqAlpha = 0 (§6)
+        }
+        lambdaP = lambda;
+        lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
+    } while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
+
+    if (!iterLimit) {
+        return NaN; // formula failed to converge
+    }
+
+    var uSq = cosSqAlpha * (a * a - b * b) / (b * b),
+        A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))),
+        B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq))),
+        deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM))),
+        s = b * A * (sigma - deltaSigma);
+    return s.toFixed(3); // round to 1mm precision
+}
 
 
 const stream = async function (req, res){
@@ -81,6 +219,7 @@ const stream = async function (req, res){
         {"value": "bounding_box: [-118.58230590820312 33.90119657968225 -118.24422607421875 34.14306652783193]"},
         {"value": "bounding_box: [13.270111083984375 52.46228526678029 13.493957519531248 52.56842095734828]"},
         ];
+
 
     /** try {
         // Gets the complete list of rules currently applied to the stream
