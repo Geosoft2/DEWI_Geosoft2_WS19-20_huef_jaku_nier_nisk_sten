a075df95c1b578ead67aa57afe776747869cc912
jan <j_sten07@uni-muenster.de>
2019-11-28 13:03:31 +0100

    first version of post / get Tweets to / from MongoDB

---
+++ api/models/tweet.js
@@ -0,0 +1,78 @@
+// jshint esversion: 6
+// jshint node: true
+"use strict";
+
+const mongoose = require('mongoose');
+
+// All parameters "required" ?
+
+// schema for media
+const MediaSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+});
+
+
+// schema for tweet
+const TweetSchema = mongoose.Schema({
+    tweetId: {
+        type: Number,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            default: 'Point'
+        },
+        coordinates: {
+            type: [[[Number]]]
+        }
+    },
+    placeName: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Mixed
+        /*
+        id: {
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+        */
+    },
+    media: {  //[MediaSchema]
+        type: mongoose.Mixed
+    }
+});
+
+TweetSchema.index({text: 'text'});
+
+module.exports = mongoose.model('Tweet', TweetSchema);
