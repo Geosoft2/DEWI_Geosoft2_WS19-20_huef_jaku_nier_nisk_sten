941b53d0efe62eba5a0b8e8ae351596ec28ac27c
Delucse <46593742+Delucse@users.noreply.github.com>
2020-01-05 18:03:52 +0100

    shows the collapsed-element corresponding to the hash

--- app/public/javascripts/faq.js
+++ app/public/javascripts/faq.js
@@ -14,10 +14,31 @@ function changeIconCollapse(){
     .on('show.bs.collapse', function(){
       $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
       $(this).prev(".card-header").find(".faq-link").addClass("font-weight-bold");
+      // window.location.hash = $(this).prev(".card-header").attr('id');
     })
     // element is hidden: change minus-icon to plus-icon & font-weight to regular
     .on('hide.bs.collapse', function(){
       $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
       $(this).prev(".card-header").find(".faq-link").removeClass("font-weight-bold");
     });
+}
+
+
+/**
+* @desc checks whether a hash is present in the URL. If yes, the function 'collapseHash' is executed.
+*/
+function isHashInUrl(){
+  if(window.location.hash) {
+    collapseHash();
+  }
+}
+
+
+/**
+* @desc shows the collapsed-element corresponding to the hash.
+*/
+function collapseHash(){
+  // puts hash in variable, and removes the # character
+  var hash = window.location.hash.substring(1);
+  $('#collapse'+hash).collapse('show');
 }
