929d71b849960545edadedf2bc00109ab2d6dd6b
phuef <phil.hueffer@gmail.com>
2020-01-27 17:16:58 +0100

    startPage Layout improved

--- app/public/javascripts/control.js
+++ app/public/javascripts/control.js
@@ -44,23 +44,6 @@ async function initial (boundingbox, events, filter) {
     await delay(1000);
 }
 
-async function fadeText(element) {
-    element.innerHTML="";
-    var op = 1;  // initial opacity
-    var timer =  setInterval(function () {
-        if (op < 0.01){
-            clearInterval(timer);
-            element.style.display = 'none';
-            fade(document.getElementById("loader-wrapper"));
-
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-
-    }, 50);
-}
-
 
 function fade(element) {
     var op = 1;  // initial opacity
